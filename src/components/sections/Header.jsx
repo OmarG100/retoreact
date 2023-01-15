4b3d51b621dfce45600e3efa3dbe1854a2fd177b
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-01-15 15:42:27 -0500

    header

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -24,85 +24,43 @@ const Header = () => {
             ref={header}
         >
 
-            <nav className="nav">
-                <div className="container f-elements f-elements--header">
-                    
-                    <img src="https://www.svgrepo.com/show/86537/airplane.svg" alt="React" width="64" height="64"
-                    className="avion__decorator" />
-                    
-                    <Link to="/">
-                        <img src={logoEAKON} alt="Logo de EAKON" className="nav__logo" width="32" height="32" />
-                    </Link>
-                    <div
-                        className="modal modal--header"
-                        ref={modal}
-                        onClick={hideMenu}
-                    >
-                        <ul
-                            className="list list--header"
-                            onClick={e => e.stopPropagation()}
-                        >
-                            <li>
-                                <NavLink
-                                    to="/productos"
-                                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                                    onClick={hideMenu}
-                                >
-                                    Productos
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/servicios"
-                                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                                    onClick={hideMenu}
-                                >
-                                    Servicios
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/nosotros"
-                                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                                    onClick={hideMenu}
-                                >
-                                    Nosotros
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink
-                                    to="/contacto"
-                                    className={({ isActive }) => isActive ? "list__link list__link--active" : "list__link"}
-                                    onClick={hideMenu}
-                                >
-                                    Contacto
-                                </NavLink>
-                            </li>
-                            <li
-                                className="icon icon--close"
-                                onClick={hideMenu}
-                            >
-                                <BsXCircle />
-                            </li>
-                        </ul>
-                    </div>
-                    <div className="f-elements f-elements--center">
-                        {/* <select>
-              <option value="value1">🌚</option>
-              <option value="value2">🌞</option>
-              <option value="value3">💻</option>
-            </select> */}
-                        <button
-                            className="icon icon--menu"
-                            onClick={showMenu}
-                        >
-                            <BsList />
-                        </button>
-                    </div>
-                </div>
-            </nav>
+<nav className="navbar navbar-expand-lg navbar-dark header">
+                        <div className="container">
+                            <a className="navbar-brand m-0 p-0" href="#">
+                                <img src="https://png.pngtree.com/png-vector/20191010/ourlarge/pngtree-travel-logo-holidays-tourism-business-trip-company-logo-design-vector-illustration-png-image_1808446.jpg"
+                                    alt="Logo Turismo" class="nav__logo" />
+                            </a>
+                            <img src="https://www.svgrepo.com/show/86537/airplane.svg" alt="React" width="64" height="64"
+                                className="avion__decorator" />
+                            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                    <li className="nav-item">
+                                        <a className="nav-link list__link" href="#hero">Inicio</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a class="nav-link list__link" href="#aboutus">Acerca de Nosotros</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link list__link" href="#contact">Contactos</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link list__link" href="#ofertas">Ofertas</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
         </header>
     );
 };
 
 export default Header;
+
+
+
+
